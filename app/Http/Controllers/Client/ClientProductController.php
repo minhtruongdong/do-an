311d15d834b105c870353ffcd4bea14914740f6c
hfao <150105438+hfao@users.noreply.github.com>
2024-12-20 18:55:24 +0700

    upload main ClienProduct

--- app/Http/Controllers/Client/ClientProductController.php
+++ app/Http/Controllers/Client/ClientProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ClientProductController extends Controller
@@ -15,8 +17,18 @@ class ClientProductController extends Controller
     }
 
     public function productdetail($id){
+        $categories = Category::with(['products' => function ($query) {
+            $query->orderBy('created_at', 'DESC')->take(5); // Lấy 5 sản phẩm mới nhất cho mỗi danh mục
+        }])->where('parent_id', 0)->get();
+        // dd($categories);
+
+        $product_detail = Product::all();
+
         return view('client.pages.product.productdetail',[
-            'id'=> $id
+            'id'=> $id,
+            'product_detail' => $product_detail,
+            'categories'=> $categories
         ]);
+
     }
 }
